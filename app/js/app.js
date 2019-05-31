452bd7ce21e6874b9f1d316308d9a50b68b92542
Cameron <cameronleecalder@gmail.com>
2019-05-30 21:20:59 -0500

    201905302120 commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,2 @@
 // Starting point for JS 
+
--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -16,15 +16,15 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get("../html/tables.html", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
+    res.sendFile(path.join(__dirname, "../html/tables.html"));
   });
 
   app.get("../html/reserve.html", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+    res.sendFile(path.join(__dirname, "../html/reserve.html"));
   });
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+    res.sendFile(path.join(__dirname, "../html/reserve.html"));
   });
 };
