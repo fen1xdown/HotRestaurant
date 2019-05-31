184d7294f6934978f3f6fce7e78579cd41e1289b
brian <roddmann3@gmail.com>
2019-05-30 20:22:04 -0500

    data file and table.js

---
+++ data/tableData.js
@@ -0,0 +1,11 @@
+var tableArray = [
+  {
+    customerName: "Billy Bob Joe",
+    customerEmail: "a=Billybj@yahoo.com",
+    customerID: "Billybobj",
+    phoneNumber: "870-267-1330"
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = tableArray;
